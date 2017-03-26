e0418dcd7ca285b05265d3b9e47ef1f5ce1f5b8b
Karan Mundra <karanmundra97@gmail.com>
2017-03-26 14:55:52 +0530

    Marcha1 incomplete solution

---
+++ Marcha1.java
@@ -0,0 +1,32 @@
+//import java.io.FileInputStream;
+//import java.io.IOException;
+//import java.util.Scanner;
+//
+///**
+// * Created by karan on 5.3.17.
+// */
+//class Marcha1 {
+//
+//    int testCases, n, m;
+//    int[]
+//    Scanner sc = new Scanner(System.in);
+//
+//    public void getInput() {
+//        int j = 0;
+//        testCases = sc.nextInt();
+//        while(j++ <testCases) {
+//            n = sc.nextInt();
+//            m = sc.nextInt();
+//            for(int i = 0;i<n;i++) {
+//
+//            }
+//            j++;
+//        }
+//
+//    }
+//
+//    public static void main(String args[]) throws IOException {
+//        System.setIn(new FileInputStream("C:\\Users\\karan\\Desktop\\codingProblems\\src\\case1.txt"));
+//        new Marcha1().getInput();
+//    }
+//}

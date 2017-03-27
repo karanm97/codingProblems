3ec27aea1e841832c9081c88dc50fd2a809e046f
Karan Mundra <karanmundra97@gmail.com>
2017-03-27 19:11:33 +0530

    Added Nukes solution

--- Nukes.java
+++ Nukes.java
@@ -10,45 +10,21 @@ import java.io.*;
 class Nukes {
 
     int A, K, N;
-    int[] chamber;
     FastReader fs = new FastReader();
+    BufferedWriter log = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    public void process() {
+    public void process() throws IOException {
         A = fs.nextInt();
         N = fs.nextInt();
         K = fs.nextInt();
-        chamber = new int[K];
-        for(int i = 0; i < A; i++) {
-            chamber[0] += 1;
-            // for (int a = 0; a < K ;a++) {
-                int a = 0;
-                while(chamber[a] > N) {
-                    chamber[a] = 0;
-                    try {
-                        chamber[a + 1] += 1;
-                        a++;
-                    } catch(Exception e) {
-                        e.printStackTrace();
-                    }
-                }
-            // }
+        for(int i = 0; i < K; i++) {
+            log.write((A % (N + 1)) + " ");
+            A /= (N + 1);
         }
-        for (int a : chamber) {
-            System.out.print(a + " ");
-        }
+        log.flush();
     }
 
-    // static int[] arrayUtil(int[] chamber, int N) {
-    //     while(chamber[i] > N) {
-    //         chamber[i] = 0;
-    //         chamber[i+1] += 1;
-    //         i++;
-    //     }
-    //     return chamber;
-    // }
-
     public static void main(String args[]) throws IOException {
-        System.setIn(new FileInputStream("d:\\programming\\case.txt"));
         new Nukes().process();
     }
 

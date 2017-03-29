5a22e5f1287728c323495f1de0ef6aa93acff864
Karan Mundra <karanmundra97@gmail.com>
2017-03-29 23:44:06 +0530

    Initial commit of Taeditor

---
+++ Taeditor.java
@@ -0,0 +1,41 @@
+class Taeditor {
+    
+    public static void main(String[] args) {
+    	FastReader fs = new FastReader();
+    	int cases = fs.nextInt();
+
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}

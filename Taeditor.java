2411b842d6080f9cf96d42b45d12df5f3aebf578
Karan Mundra <karanmundra97@gmail.com>
2017-03-30 23:49:37 +0530

    To Solve - NZEC error

--- Taeditor.java
+++ Taeditor.java
@@ -1,18 +1,26 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 class Taeditor {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        //        System.setIn(new FileInputStream("C:\\Users\\karan\\Desktop\\codingProblems\\src\\case.txt"));
         FastReader fs = new FastReader();
         int cases = fs.nextInt();
-        StringBuilder S = new StringBuilder();
-        while(cases-- > 0) {
-            StringBuilder query = fs.nextLine();
-            if(query.charAt(0) == '+') {
-       			
-       			
+        StringBuilder mainString = new StringBuilder();
+        StringBuilder printStream = new StringBuilder();
+        while (cases-- > 0) {
+            String query[] = fs.nextLine().split(" ");
+            if (query[0].equals("+")) {
+                mainString.insert(Integer.parseInt(query[1]), query[2]);
             } else {
-       
+                printStream.append(mainString.substring(Integer.parseInt(query[1]) - 1, Integer.parseInt(query[2]))).append("\n");
             }
         }
+        System.out.println(printStream);
     }
 
     static class FastReader {
@@ -22,6 +30,7 @@ class Taeditor {
         public FastReader() {
             br = new BufferedReader(new InputStreamReader(System.in));
         }
+
         String next() {
             while (st == null || !st.hasMoreElements()) {
                 try {

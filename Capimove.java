be1159883b35949222ed7be640214aeb6b0c6ad8
Karan Mundra <karanmundra97@gmail.com>
2017-04-27 14:49:25 +0530

    Changed the Data Structures

--- Capimove.java
+++ Capimove.java
@@ -6,7 +6,42 @@ class Capimove {
 
     public static void main(String[] args) throws IOException {
         System.setIn(new FileInputStream("d:\\programming\\case.txt"));
-        
+        FastReader fs = new FastReader();
+        int testCases = fs.nextInt();
+        while(testCases-- > 0) {
+            int planetCount = fs.nextInt();
+            Set<Integer> set = new HashSet<Integer>();
+            Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+            PriorityQueue<Integer> queue = new PriorityQueue<Integer>((x,y) -> y-x);
+            for(int i = 1; i <= planetCount; i++) {
+                int item = fs.nextInt();
+                queue.add(item);
+                map.put(item, i);
+            }
+            int[][] adjMatrix = new int[planetCount + 1][planetCount + 1];
+            for(int i = 1; i <= planetCount - 1; i++) {
+                int V = fs.nextInt();
+                int U = fs.nextInt();
+                adjMatrix[V][U] = 1;
+                adjMatrix[U][V] = 1;
+                adjMatrix[i][i] = 1;
+            }
+            for (int i = 0; i <= planetCount; i++ ) {
+                for (int j = 0; j <= planetCount; j++) {
+                    System.out.print(adjMatrix[i][j] + " ");
+                }
+                System.out.println();
+            }
+            for(int i = 1; i <= planetCount; i++) {
+                PriorityQueue<Integer> copyQueue = new PriorityQueue<Integer>(queue);
+                // System.out.println(copyQueue.peek());
+                int temp = map.get(copyQueue.poll());
+                if(adjMatrix[i][temp]!=0){
+                    System.out.println(temp);
+                }
+                   
+            }
+        }
     }
 
     static class FastReader {

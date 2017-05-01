7f8516f886962bad4ca47cc704ce7a91b56e7f2a
Karan Mundra <karanmundra97@gmail.com>
2017-05-01 23:21:19 +0530

    Re-structured Marcha1

--- Marcha1.java
+++ Marcha1.java
@@ -1,32 +1,62 @@
-//import java.io.FileInputStream;
-//import java.io.IOException;
-//import java.util.Scanner;
-//
-///**
-// * Created by karan on 5.3.17.
-// */
-//class Marcha1 {
-//
-//    int testCases, n, m;
-//    int[]
-//    Scanner sc = new Scanner(System.in);
-//
-//    public void getInput() {
-//        int j = 0;
-//        testCases = sc.nextInt();
-//        while(j++ <testCases) {
-//            n = sc.nextInt();
-//            m = sc.nextInt();
-//            for(int i = 0;i<n;i++) {
-//
-//            }
-//            j++;
-//        }
-//
-//    }
-//
-//    public static void main(String args[]) throws IOException {
-//        System.setIn(new FileInputStream("C:\\Users\\karan\\Desktop\\codingProblems\\src\\case1.txt"));
-//        new Marcha1().getInput();
-//    }
-//}
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Marcha1 {
+
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("d:\\programming\\case.txt"));
+        FastReader fs = new FastReader();
+        int testCases = fs.nextInt();
+        while(testCases-- > 0) {
+            int noteCount = fs.nextInt();
+            int muggerAmount = fs.nextInt();
+            for(int i = 0; i < noteCount; i++) {
+            	int temp = fs.nextInt();
+            	if(temp < muggerAmount)
+            }
+        }
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException  e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}

ac1d17fe90bbc2480412c542692166e6adbc3f51
Karan Mundra <karanmundra97@gmail.com>
2017-05-02 18:26:03 +0530

    Added Icpc16 (Wrong Answer)

--- Icpc16b.java
+++ Icpc16b.java
@@ -1,61 +1,82 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+/*
+Solution of Codechef Problem - Beautiful Arrays
+Problem Code - ICPC16B
+Link - https://www.codechef.com/problems/ICPC16B
+*/
 
-public class Icpc16b {
+import java.util.*;
+import java.io.*;
+import java.lang.*;
 
-  public static void main(String args[]) {
-    new Icpc16b().process();
-  }
+class Icpc16b {
 
-  public void process() {
-    int[] input_array = {0, 1, 2, 3, 4, 5, 6, 7};
-    OutermostLoop:
-    for (int i = 0; i < input_array.length - 1; i++) {
-      //            System.out.println("First For Loop");
-      for (int j = 1; j < input_array.length; j++) {
-        //                System.out.println("Second For Loop");
-
-        for (int k = j; k < input_array.length; k++) {
-
-          if (input_array[k] == (input_array[i] * input_array[j])) {
-            System.out.println("andar hai" + i + " " + j + " " + k);
-
-            if (i == input_array.length - 1 && j == input_array.length) System.out.println("yes");
-            continue;
-          }
-          if (k == input_array.length) System.out.println("no");
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("d:\\programming\\case.txt"));
+        FastReader fs = new FastReader();
+        int testCases = fs.nextInt();
+        while(testCases-- > 0) {
+            int N = fs.nextInt();
+            List<Long> list = new ArrayList<Long>();
+            boolean doesContain = true, containsOne =  false;
+            for(int i = 0; i < N; i++) {
+                list.add(fs.nextLong());
+            }
+            Collections.sort(list);
+            for(int i = 0; i < N - 1; i++) {
+                for(int j = i + 1; j < N; j++) {
+                    if (!(Collections.binarySearch(list, list.get(i) * list.get(j)) >= 0)) {
+                        doesContain = false;
+                        break;
+                    }
+                }
+                if(doesContain) {
+                    System.out.println("yes");
+                } else {
+                    System.out.println("no");
+                }
+            }
         }
-      }
-    }
-  }
-
-  static class FastReader {
-    BufferedReader br;
-    StringTokenizer st;
-
-    public FastReader() {
-      br = new BufferedReader(new InputStreamReader(System.in));
     }
 
-    String next() {
-      while (st == null || !st.hasMoreElements()) {
-        try {
-          st = new StringTokenizer(br.readLine());
-        } catch (IOException e) {
-          e.printStackTrace();
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
         }
-      }
-      return st.nextToken();
-    }
 
-    int nextInt() {
-      return Integer.parseInt(next());
-    }
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException  e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
 
-    long nextLong() {
-      return Long.parseLong(next());
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
     }
-  }
 }

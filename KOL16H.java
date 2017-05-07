7ceda4ba4a5a120610545e1ba488115141b1d808
Karan Mundra <karanmundra97@gmail.com>
2017-05-07 18:06:18 +0530

    Added KOL16H solution

--- KOL16H.java
+++ KOL16H.java
@@ -1,16 +1,26 @@
+/*
+Solution of Codechef Problem - Mr Wireless
+Problem Code - KOL16H
+Link - https://www.codechef.com/problems/KOL16H
+*/
+
 import java.util.*;
 import java.io.*;
 import java.lang.*;
+import java.math.*;
 
 class KOL16H {
 
     public static void main(String[] args) throws IOException {
-        System.setIn(new FileInputStream("d:\\programming\\case.txt"));
         FastReader fs = new FastReader();
         int testCases = fs.nextInt();
+        int caseNumber = 1;
+        final BigInteger modValue = new BigInteger("8589934592");
+        final BigInteger valueTwo = new BigInteger("2");
         while(testCases-- > 0) {
-                  
-
+            BigInteger pow = new BigInteger(fs.nextLine());
+            BigInteger result = valueTwo.modPow(pow, modValue).subtract(BigInteger.ONE).add(modValue).mod(modValue);
+            System.out.println("Case " + caseNumber++ + ": " + result);
         }
     }
 

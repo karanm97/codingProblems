18afc76f26941f6dd4bae5d803e1a8833b069c88
Karan Mundra <karanmundra97@gmail.com>
2017-05-08 16:01:15 +0530

    Initial commit

---
+++ CAPIMOVE.java
@@ -0,0 +1,56 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Capimove {
+
+    public static void main(String[] args) throws IOException {
+        FastReader fs = new FastReader();
+        int testCases = fs.nextInt();
+        while(testCases-- > 0) {
+            
+        }
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException  e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}

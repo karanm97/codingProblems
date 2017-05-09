d2cdc812c0ea14b08658f2849362e36c3f501f73
Karan Mundra <karanmundra97@gmail.com>
2017-05-09 07:05:04 +0530

    Added MXMEDIAN solution

--- MXMEDIAN.java
+++ MXMEDIAN.java
@@ -10,15 +10,27 @@ class MXMEDIAN {
         int testCases = fs.nextInt();
         while(testCases-- > 0) {
             int N = fs.nextInt();
-            int[] array = new int[2 * N];
+            Queue<Integer> queue = new PriorityQueue<Integer>((x, y) -> y - x);
             for(int i = 0; i < 2 * N; i++) {
-                array[i] = fs.nextInt();
+                queue.add(fs.nextInt());
             }
-            Arrays.sort(array);
-            Queue<Integer> queue = new PriorityQueue<Integer>();
-            for(int i = 2 * N - 1; i > N - 1; i++) {
-            	queue.add(array[i]);
+            StringBuilder string = new StringBuilder();
+            int[] array = new int[2 * N];
+            int k = 0;
+            for(int i = 1; i <= 2 * N - 1; i += 2) {
+                if(i == N) {
+                    string.append(queue.peek()).append("\n");
+                }
+                array[i] = queue.poll();
+                k++;
             }
+            for(int i = 0; i <= 2 * N - 2; i += 2) {
+                array[i] = queue.poll();
+            }
+            for(int a : array) {
+                string.append(a).append(" ");
+            }
+            System.out.println(string.toString());
         }
     }
 

6769c213a75d2fdb8a5d5ac8afcd6528a09d1911
Karan Mundra <karanmundra97@gmail.com>
2017-05-10 23:55:24 +0530

    Incomplete solution

--- CBALLS.java
+++ CBALLS.java
@@ -1,3 +1,9 @@
+/*
+Solution of Codechef Problem - Churu and Balls
+Problem Code - CBALLS
+Link - https://www.codechef.com/problems/CBALLS
+*/
+
 import java.util.*;
 import java.io.*;
 import java.lang.*;
@@ -9,9 +15,36 @@ class CBALLS {
         FastReader fs = new FastReader();
         int testCases = fs.nextInt();
         while(testCases-- > 0) {
-                  
-
+            int N = fs.nextInt();
+            int[] array = new int[N];
+            boolean[] utilArray = new boolean[N];
+            for(int i = 0; i < N; i++) {
+                array[i] = fs.nextInt();
+            }
+            for (int i = 0; i < N; i++) {
+            	if(isPrime(array[i])) {
+            		utilArray[i] = true;
+            	}
+            }
         }
+    }
+
+    public static boolean isPrime(int number) {
+        if(number < 2) {
+            return false;
+        }
+        if(number == 2) {
+            return true;
+        }
+        if(number % 2 == 0) {
+            return false;
+        }
+        for(int i = 3; i <= Math.sqrt(number); i += 2) {
+            if(number % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 
     static class FastReader {

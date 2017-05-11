ec78c063334afa09dfd7e1ab66599836007ee5d0
Karan Mundra <karanmundra97@gmail.com>
2017-05-11 23:07:21 +0530

    Local test cases working

--- BINTREE.java
+++ BINTREE.java
@@ -9,9 +9,54 @@ class BINTREE {
         FastReader fs = new FastReader();
         int queryCount = fs.nextInt();
         while(queryCount-- > 0) {
-        	long a = fs.nextLong();
-        	long b = fs.nextLong();
+            long numberOne = fs.nextLong();
+            long numberTwo = fs.nextLong();
+            long levelOfNumberOne = (long) (Math.log(numberOne) / Math.log(2L));
+            long levelOfNumberTwo = (long) (Math.log(numberTwo) / Math.log(2L));
+            boolean isNumOneOnLeftPart = checkIfLeftHalf(numberOne);
+            boolean isNumTwoOnLeftPart = checkIfLeftHalf(numberTwo);
+            long difference = Math.abs(levelOfNumberTwo - levelOfNumberOne);
+            if(difference > 1) {
+                if(isNumOneOnLeftPart == isNumTwoOnLeftPart) {
+                    System.out.println(difference);
+                } else {
+                    System.out.println(difference + 2);
+                }
+            } else if(difference == 0) {
+                if(isNumTwoOnLeftPart == isNumTwoOnLeftPart) {
+                    System.out.println("2");
+                } else {
+                    System.out.println(2 * levelOfNumberTwo);
+                }
+            } else if(difference == 1) {
+                long max, min;
+                if(numberOne > numberTwo) {
+                    max = numberOne;
+                    min = numberTwo;
+                } else {
+                    min = numberOne;
+                    max = numberTwo;
+                }
+                if(isNumTwoOnLeftPart == isNumTwoOnLeftPart) {
+                    if(max == 2 * min || max == 2 * min + 1) {
+                        System.out.println("1");
+                    } else {
+                        System.out.println("3");
+                    }
+                } else {
+                    System.out.println(levelOfNumberTwo + levelOfNumberTwo);
+                }
+            }
+        }
+    }
 
+    public static boolean checkIfLeftHalf(long number) {
+        long minOfLevel = (long) Math.pow(2, number);
+        long maxOfLevel = minOfLevel + minOfLevel / 2 - 1;
+        if(number >= minOfLevel && number <= maxOfLevel) {
+            return true;
+        } else {
+            return false;
         }
     }
 

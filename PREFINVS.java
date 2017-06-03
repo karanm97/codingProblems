6ddbddb2935807978e228baf2d827a9d0ac2e45e
Karan Mundra <karanmundra97@gmail.com>
2017-06-03 16:47:53 +0530

    Optimized solution

--- PREFINVS.java
+++ PREFINVS.java
@@ -10,9 +10,16 @@ import java.lang.*;
 
 class PREFINVS {
 
+    private static InputStream stream;
+    private static byte[] buf = new byte[1024];
+    private static int curChar;
+    private static int numChars;
+    private static SpaceCharFilter filter;
+    static BufferedWriter log = new BufferedWriter(new OutputStreamWriter(System.out));
+
     public static void main(String[] args) throws IOException {
-        FastReader fs = new FastReader();
-        String inputString = fs.nextLine();
+        InputReader(System.in);
+        String inputString = nLi();
         int index = inputString.length() - 1;
         int counter = 0, number = 0;
         for(int i = index; i >= 0; i--) {
@@ -27,48 +34,119 @@ class PREFINVS {
                 }
             }
         }
-        System.out.println(counter);
+        log.write(String.valueOf(counter));
+        log.flush();
     }
 
-    static class FastReader {
-        BufferedReader br;
-        StringTokenizer st;
+    public static void InputReader(InputStream stream1) {
+        stream = stream1;
+    }
 
-        public FastReader() {
-            br = new BufferedReader(new InputStreamReader(System.in));
+    private static boolean isWhitespace(int c) {
+        return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+    }
+
+    private static boolean isEndOfLine(int c) {
+        return c == '\n' || c == '\r' || c == -1;
+    }
+
+    private static int read() {
+        if (numChars == -1) {
+            throw new InputMismatchException();
         }
-
-        String next() {
-            while (st == null || !st.hasMoreElements()) {
-                try {
-                    st = new StringTokenizer(br.readLine());
-                } catch (IOException  e) {
-                    e.printStackTrace();
-                }
-            }
-            return st.nextToken();
-        }
-
-        int nextInt() {
-            return Integer.parseInt(next());
-        }
-
-        long nextLong() {
-            return Long.parseLong(next());
-        }
-
-        double nextDouble() {
-            return Double.parseDouble(next());
-        }
-
-        String nextLine() {
-            String str = "";
+        if (curChar >= numChars) {
+            curChar = 0;
             try {
-                str = br.readLine();
+                numChars = stream.read(buf);
             } catch (IOException e) {
-                e.printStackTrace();
+                throw new InputMismatchException();
             }
-            return str;
+            if (numChars <= 0) {
+                return -1;
+            }
         }
+        return buf[curChar++];
+    }
+
+    private static int nI() {
+        int c = read();
+        while (isSpaceChar(c)) {
+            c = read();
+        }
+        int sgn = 1;
+        if (c == '-') {
+            sgn = -1;
+            c = read();
+        }
+        int res = 0;
+        do {
+            if (c < '0' || c > '9') {
+                throw new InputMismatchException();
+            }
+            res *= 10;
+            res += c - '0';
+            c = read();
+        } while (!isSpaceChar(c));
+        return res * sgn;
+    }
+
+    private static long nL() {
+        int c = read();
+        while (isSpaceChar(c)) {
+            c = read();
+        }
+        int sgn = 1;
+        if (c == '-') {
+            sgn = -1;
+            c = read();
+        }
+        long res = 0;
+        do {
+            if (c < '0' || c > '9') {
+                throw new InputMismatchException();
+            }
+            res *= 10;
+            res += c - '0';
+            c = read();
+        } while (!isSpaceChar(c));
+        return res * sgn;
+    }
+
+    private static String nS() {
+        int c = read();
+        while (isSpaceChar(c)) {
+            c = read();
+        }
+        StringBuilder res = new StringBuilder();
+        do {
+            res.appendCodePoint(c);
+            c = read();
+        } while (!isSpaceChar(c));
+        return res.toString();
+    }
+
+    private static String nLi() {
+        int c = read();
+        while (isSpaceChar(c)) {
+            c = read();
+        }
+        StringBuilder res = new StringBuilder();
+        do {
+            res.appendCodePoint(c);
+            c = read();
+        } while (!isEndOfLine(c));
+        return res.toString();
+    }
+
+    private static boolean isSpaceChar(int c) {
+        if (filter != null) {
+            return filter.isSpaceChar(c);
+        }
+        return isWhitespace(c);
+    }
+
+    private interface SpaceCharFilter {
+
+        public boolean isSpaceChar(int ch);
     }
 }

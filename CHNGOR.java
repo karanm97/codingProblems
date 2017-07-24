eb8c4f3a341f21772f45881cf37882ac49b2a007
Karan Mundra <karanmundra97@gmail.com>
2017-07-24 19:01:33 +0530

    Added CHNGOR solution

---
+++ CHNGOR.java
@@ -0,0 +1,156 @@
+/*
+Solution of Codechef Problem - Chang and Bitwise OR
+Problem Code - CHNGOR
+Link - https://www.codechef.com/problems/CHNGOR
+*/
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class CHNGOR {
+
+    private static InputStream stream;
+    private static byte[] buf = new byte[1024];
+    private static int curChar;
+    private static int numChars;
+    private static SpaceCharFilter filter;
+    static BufferedWriter log = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        InputReader(System.in);
+        int testCases = nI();
+        PriorityQueue<Integer> queue;
+        int n, a, b;
+        long sum;
+        while(testCases-- > 0) {
+            n = nI();
+            sum = 0;
+            queue = new PriorityQueue<>();
+            for(int i = 0; i < n; i++) {
+                queue.add(nI());
+            }
+            while(!queue.isEmpty()) {
+                a = queue.poll();
+                b = 0;
+                if(sum != a) {
+                    sum |= a;
+                }
+            }
+            log.write(String.valueOf(sum) + "\n");
+            log.flush();
+        }
+    }
+
+    public static void InputReader(InputStream stream1) {
+        stream = stream1;
+    }
+
+    private static boolean isWhitespace(int c) {
+        return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+    }
+
+    private static boolean isEndOfLine(int c) {
+        return c == '\n' || c == '\r' || c == -1;
+    }
+
+    private static int read() {
+        if (numChars == -1) {
+            throw new InputMismatchException();
+        }
+        if (curChar >= numChars) {
+            curChar = 0;
+            try {
+                numChars = stream.read(buf);
+            } catch (IOException e) {
+                throw new InputMismatchException();
+            }
+            if (numChars <= 0) {
+                return -1;
+            }
+        }
+        return buf[curChar++];
+    }
+
+    private static int nI() {
+        int c = read();
+        while (isSpaceChar(c)) {
+            c = read();
+        }
+        int sgn = 1;
+        if (c == '-') {
+            sgn = -1;
+            c = read();
+        }
+        int res = 0;
+        do {
+            if (c < '0' || c > '9') {
+                throw new InputMismatchException();
+            }
+            res *= 10;
+            res += c - '0';
+            c = read();
+        } while (!isSpaceChar(c));
+        return res * sgn;
+    }
+
+    private static long nL() {
+        int c = read();
+        while (isSpaceChar(c)) {
+            c = read();
+        }
+        int sgn = 1;
+        if (c == '-') {
+            sgn = -1;
+            c = read();
+        }
+        long res = 0;
+        do {
+            if (c < '0' || c > '9') {
+                throw new InputMismatchException();
+            }
+            res *= 10;
+            res += c - '0';
+            c = read();
+        } while (!isSpaceChar(c));
+        return res * sgn;
+    }
+
+    private static String nS() {
+        int c = read();
+        while (isSpaceChar(c)) {
+            c = read();
+        }
+        StringBuilder res = new StringBuilder();
+        do {
+            res.appendCodePoint(c);
+            c = read();
+        } while (!isSpaceChar(c));
+        return res.toString();
+    }
+
+    private static String nLi() {
+        int c = read();
+        while (isSpaceChar(c)) {
+            c = read();
+        }
+        StringBuilder res = new StringBuilder();
+        do {
+            res.appendCodePoint(c);
+            c = read();
+        } while (!isEndOfLine(c));
+        return res.toString();
+    }
+
+    private static boolean isSpaceChar(int c) {
+        if (filter != null) {
+            return filter.isSpaceChar(c);
+        }
+        return isWhitespace(c);
+    }
+
+    private interface SpaceCharFilter {
+
+        public boolean isSpaceChar(int ch);
+    }
+}

e49683e6c3fe5d56526d2e18f5afcf6f80b1a6ab
Karan Mundra <karanmundra97@gmail.com>
2017-11-05 23:05:00 +0530

    Added VILTRIBE solution

---
+++ VILTRIBE.java
@@ -0,0 +1,171 @@
+/*
+Solution of Codechef Problem - Villages and Tribes
+Problem Code - VILTRIBE
+Link - https://www.codechef.com/problems/VILTRIBE
+*/
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class VILTRIBE {
+
+    private static InputStream stream;
+    private static byte[] buf = new byte[1024];
+    private static int curChar;
+    private static int numChars;
+    private static SpaceCharFilter filter;
+    static BufferedWriter log = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        InputReader(System.in);
+        int testCases = nI();
+        while(testCases-- > 0) {
+            String inputString = nLi();
+            int length = inputString.length();
+            int countA = 0, countB = 0, temp = 0;
+            char start = 'Q';
+            if(inputString.charAt(0) == 'A') {
+                start = 'Z';
+                countA++;
+            } else if(inputString.charAt(0) == 'B') {
+                start = 'Z';
+                countB++;
+            }
+            for(int i = 1; i < length; i++) {
+                if(inputString.charAt(i) == 'A') {
+                    countA++;
+                    if(temp != 0 && start == 'A') {
+                        countA += temp;
+                    }
+                    temp = 0;
+                } else if(inputString.charAt(i) == 'B') {
+                    countB++;
+                    if(temp != 0 && start == 'B') {
+                        countB += temp;
+                    }
+                    temp = 0;
+                } else if(inputString.charAt(i) == '.') {
+                    if(temp == 0) {
+                        start = inputString.charAt(i - 1);
+                    }
+                    temp++;
+                }
+            }
+            log.write(String.valueOf(countA + " " + countB + "\n"));
+            log.flush();
+        }
+    }
+
+    public static void InputReader(InputStream stream1) {
+        stream = stream1;
+    }
+
+    private static boolean isWhitespace(int c) {
+        return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+    }
+
+    private static boolean isEndOfLine(int c) {
+        return c == '\n' || c == '\r' || c == -1;
+    }
+
+    private static int read() {
+        if (numChars == -1) {
+            throw new InputMismatchException();
+        }
+        if (curChar >= numChars) {
+            curChar = 0;
+            try {
+                numChars = stream.read(buf);
+            } catch (IOException e) {
+                throw new InputMismatchException();
+            }
+            if (numChars <= 0) {
+                return -1;
+            }
+        }
+        return buf[curChar++];
+    }
+
+    private static int nI() {
+        int c = read();
+        while (isSpaceChar(c)) {
+            c = read();
+        }
+        int sgn = 1;
+        if (c == '-') {
+            sgn = -1;
+            c = read();
+        }
+        int res = 0;
+        do {
+            if (c < '0' || c > '9') {
+                throw new InputMismatchException();
+            }
+            res *= 10;
+            res += c - '0';
+            c = read();
+        } while (!isSpaceChar(c));
+        return res * sgn;
+    }
+
+    private static long nL() {
+        int c = read();
+        while (isSpaceChar(c)) {
+            c = read();
+        }
+        int sgn = 1;
+        if (c == '-') {
+            sgn = -1;
+            c = read();
+        }
+        long res = 0;
+        do {
+            if (c < '0' || c > '9') {
+                throw new InputMismatchException();
+            }
+            res *= 10;
+            res += c - '0';
+            c = read();
+        } while (!isSpaceChar(c));
+        return res * sgn;
+    }
+
+    private static String nS() {
+        int c = read();
+        while (isSpaceChar(c)) {
+            c = read();
+        }
+        StringBuilder res = new StringBuilder();
+        do {
+            res.appendCodePoint(c);
+            c = read();
+        } while (!isSpaceChar(c));
+        return res.toString();
+    }
+
+    private static String nLi() {
+        int c = read();
+        while (isSpaceChar(c)) {
+            c = read();
+        }
+        StringBuilder res = new StringBuilder();
+        do {
+            res.appendCodePoint(c);
+            c = read();
+        } while (!isEndOfLine(c));
+        return res.toString();
+    }
+
+    private static boolean isSpaceChar(int c) {
+        if (filter != null) {
+            return filter.isSpaceChar(c);
+        }
+        return isWhitespace(c);
+    }
+
+    private interface SpaceCharFilter {
+
+        public boolean isSpaceChar(int ch);
+    }
+}

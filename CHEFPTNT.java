259b50ba9a215b59e1175cbdaa554b22ca8983ad
Karan Mundra <karanmundra97@gmail.com>
2018-02-09 13:46:10 +0530

    Added CHEFPTNT solution

---
+++ CHEFPTNT.java
@@ -0,0 +1,174 @@
+/*
+Solution of Codechef Problem - Chef And The Patents
+Problem Code - CHEFPTNT
+Link - https://www.codechef.com/problems/CHEFPTNT
+*/
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class CHEFPTNT {
+
+    private static InputStream stream;
+    private static byte[] buf = new byte[1024];
+    private static int curChar;
+    private static int numChars;
+    private static SpaceCharFilter filter;
+    static BufferedWriter log = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        InputReader(System.in);
+        int testCases = nI();
+        while(testCases-- > 0) {
+            int n = nI();
+            int m = nI();
+            int x = nI();
+            int k = nI();
+            String str = nLi();
+            int evenCount = 0, oddCount = 0;
+            for(int i = 0; i < str.length(); i++) {
+                if(str.charAt(i) == 'E') {
+                    evenCount++;
+                } else {
+                    oddCount++;
+                }
+            }
+            if(x == 0 || m == 0) {
+                log.write("no\n");
+                continue;
+            }
+            int patents = 0;
+            int minOddWork = 0, minEvenWork = 0;
+            for(int i = 1; i <= m; i++) {
+                if(i % 2 != 0) {
+                    minOddWork = Math.min(x, oddCount);
+                    patents += minOddWork;
+                    oddCount -= minOddWork;
+                } else  {
+                    minEvenWork = Math.min(x, evenCount);
+                    patents += minEvenWork;
+                    evenCount -= minEvenWork;
+                }
+            }
+            if(patents - n >= 0) {
+                log.write("yes\n");
+            } else {
+                log.write("no\n");
+            }
+            log.flush();
+        }
+    }
+
+    public static void InputReader(InputStream stream1) {
+        stream = stream1;
+    }
+
+    private static boolean isWhitespace(int c) {
+        return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+    }
+
+    private static boolean isEndOfLine(int c) {
+        return c == '\n' || c == '\r' || c == -1;
+    }
+
+    private static int read() {
+        if (numChars == -1) {
+            throw new InputMismatchException();
+        }
+        if (curChar >= numChars) {
+            curChar = 0;
+            try {
+                numChars = stream.read(buf);
+            } catch (IOException e) {
+                throw new InputMismatchException();
+            }
+            if (numChars <= 0) {
+                return -1;
+            }
+        }
+        return buf[curChar++];
+    }
+
+    private static int nI() {
+        int c = read();
+        while (isSpaceChar(c)) {
+            c = read();
+        }
+        int sgn = 1;
+        if (c == '-') {
+            sgn = -1;
+            c = read();
+        }
+        int res = 0;
+        do {
+            if (c < '0' || c > '9') {
+                throw new InputMismatchException();
+            }
+            res *= 10;
+            res += c - '0';
+            c = read();
+        } while (!isSpaceChar(c));
+        return res * sgn;
+    }
+
+    private static long nL() {
+        int c = read();
+        while (isSpaceChar(c)) {
+            c = read();
+        }
+        int sgn = 1;
+        if (c == '-') {
+            sgn = -1;
+            c = read();
+        }
+        long res = 0;
+        do {
+            if (c < '0' || c > '9') {
+                throw new InputMismatchException();
+            }
+            res *= 10;
+            res += c - '0';
+            c = read();
+        } while (!isSpaceChar(c));
+        return res * sgn;
+    }
+
+    private static String nS() {
+        int c = read();
+        while (isSpaceChar(c)) {
+            c = read();
+        }
+        StringBuilder res = new StringBuilder();
+        do {
+            res.appendCodePoint(c);
+            c = read();
+        } while (!isSpaceChar(c));
+        return res.toString();
+    }
+
+    private static String nLi() {
+        int c = read();
+        while (isSpaceChar(c)) {
+            c = read();
+        }
+        StringBuilder res = new StringBuilder();
+        do {
+            res.appendCodePoint(c);
+            c = read();
+        } while (!isEndOfLine(c));
+        return res.toString();
+    }
+
+    private static boolean isSpaceChar(int c) {
+        if (filter != null) {
+            return filter.isSpaceChar(c);
+        }
+        return isWhitespace(c);
+    }
+
+    private interface SpaceCharFilter {
+
+        public boolean isSpaceChar(int ch);
+    }
+}
